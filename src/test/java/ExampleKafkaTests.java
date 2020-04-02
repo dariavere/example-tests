9b8b7d4388103398c1854621eadcecfc4394b0c9
DV <dariavere@gmail.com>
2020-04-02 12:25:08 +0300

    add kafka test

--- src/test/java/ExampleKafkaTests.java
+++ src/test/java/ExampleKafkaTests.java
@@ -1,9 +1,15 @@
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.junit.Rule;
 import org.junit.Test;
 import org.testcontainers.containers.KafkaContainer;
 
 
 import java.time.Duration;
+import java.util.*;
+
+import static org.apache.kafka.clients.admin.AdminClient.*;
 
 
 public class ExampleKafkaTests {
@@ -15,7 +21,18 @@ public class ExampleKafkaTests {
 
 
     @Test
-    public void kafkaStartTest() {
-        kafka.getBootstrapServers();
+    public void kafkaStartTest() throws Exception {
+
+        Collection<NewTopic> collection = Collections.singleton(new NewTopic("testTopic", 1, (short) 1));
+
+        AdminClient kafkaAdminClient = create(Collections.singletonMap("bootstrap.servers", kafka.getBootstrapServers()));
+
+        int kafkaPort = kafka.getMappedPort(9093);
+        int zookeeperPort = kafka.getMappedPort(2181);
+
+        CreateTopicsResult createTopicsResult = kafkaAdminClient.createTopics(collection);
+
+        createTopicsResult.all().get();
+
     }
 }
